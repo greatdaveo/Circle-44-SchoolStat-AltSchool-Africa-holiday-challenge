8a85b189579937bb1e49fdc881e68b4b4ce1454c
Olagunju kolade <97587019+kodeman2@users.noreply.github.com>
2022-07-26 16:01:10 +0000

    general styling

---
+++ index.js
@@ -0,0 +1,22 @@
+//adding hovered class to selected list item
+let list = document.querySelectorAll("#nav-container li");
+
+function activeLink(){
+  list.forEach((item) => {
+   item.classList.remove("hovered");
+  });
+  this.classList.add("hovered");
+}
+
+list.forEach(item => item.addEventListener("mouseover", activeLink));
+
+//Menu Toggle 
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector("#nav-container");
+let main = document.querySelector(".main");
+
+toggle.onclick = function () {
+ navigation.classList.toggle("active");
+ main.classList.toggle("active");
+};
+
